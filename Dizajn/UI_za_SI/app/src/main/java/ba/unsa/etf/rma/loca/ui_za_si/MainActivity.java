35a00d4acaab628a4bb77d615c033d907c335844
alocic1 <alocic1@etf.unsa.ba>
2018-04-06 02:05:35 +0200

    UI XML

---
+++ Dizajn/UI_za_SI/app/src/main/java/ba/unsa/etf/rma/loca/ui_za_si/MainActivity.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rma.loca.ui_za_si;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
