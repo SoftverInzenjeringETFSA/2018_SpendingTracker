670928ca913051ed26b8ed21c7bd4426e4d2f908
hhelja1 <hhelja1@etf.unsa.ba>
2018-05-09 22:49:06 +0200

    + kod za kreiranje baze (nije testirano)

--- SpendingTracker/nodejs-assets/nodejs-project/main.js
+++ SpendingTracker/nodejs-assets/nodejs-project/main.js
@@ -10,3 +10,42 @@ rn_bridge.channel.on('message', (msg) => {
 
 // Inform react-native node is initialized.
 rn_bridge.channel.send("Node was initialized.");
+
+//dio za kreiranje baze 
+var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb://localhost:27017/SpendingTracker";
+
+MongoClient.connect(url, function(err,db){
+  if (err) throw err;
+  console.log("Database created");
+  db.close();
+});
+
+mongoose.connect("mongodb://localhost:27017/SpendingTracker");
+
+var Schema = mongoose.Schema;
+
+var korisnikSchema = new Schema(
+  { ime: String, 
+      prezime: String,
+      lozinka: String,
+      mjesecniPrihod: Number,
+      troskovniLimit: Number,
+      valuta: String,
+      email: String,
+      racuni: [
+        { naziv: String, 
+          trenutniIznos: Number,
+          troskovi: [
+            { iznos: Number,
+              kategorija: {naziv: String}
+            }
+          ]
+        }
+      ]
+    }
+);
+
+var korisnik = mongoose.model('korisnik', korisnikSchema);
