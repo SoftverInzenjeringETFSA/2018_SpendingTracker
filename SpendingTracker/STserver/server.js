1bb215d4788190939d3f5f538e85c6cbe838d516
hhelja1 <hhelja1@etf.unsa.ba>
2018-05-17 12:30:03 +0200

    dodane validacije u šemu mongodb

--- SpendingTracker/STserver/server.js
+++ SpendingTracker/STserver/server.js
@@ -10,23 +10,89 @@ var db = mongoose.connection;
 var Schema = mongoose.Schema;
 
 var korisnikSchema = new Schema(
-  {   ime: String, 
-      prezime: String,
-      lozinka: String,
+  {   ime: {
+        type: String,
+        validate: {
+          validator: function(value){
+            return /^[A-Z][a-z]{1,19}$/.test(value);
+          },
+          message: 'Ime mora počinjati velikim slovom i ne može sadržavati specijalne karaktere!'
+        },
+        required: [true, 'Obavezno je unijeti ime!']  
+      }, 
+      prezime: {
+        type: String,
+        validate: {
+          validator: function(value){
+            return /^[A-Z][a-z]{1,19}$/.test(value);
+          },
+          message: 'Prezime mora počinjati velikim slovom i ne može sadržavati specijalne karaktere!'
+        },
+        required: [true, 'Obavezno je unijeti prezime!'] 
+      }, 
+      lozinka: {
+        type: String,
+        validate: {
+          validator: function(value){
+            return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{7,20}$/.test(value); //bar jedno veliko slovo, jedna cifra, jedno malo slovo i duzina izmedju 7 i 20
+          },
+          message: 'Lozinka mora sadržavati bar 7 karaktera, od kojih bar jedno veliko slovo, bar jedno malo slovo i bar jednu cifru!'
+        },
+        required: [true, 'Obavezno je unijeti lozinku!'] 
+      }, 
       mjesecniPrihod: Number,
       troskovniLimit: Number,
       valuta: String,
-      email: String,
+      email: {
+        type: String,
+        validate: {
+          validator: function(value){
+            return /^(([^<>()\[\]\\.,;:\s@"]+((\.(?!\.))*[^<>()\[\]\\.,;:\s@"]+)*))@[^<>()\[\]\\.,;:\s@_\-"]+\.[^<>()\[\]\\.,;:\s@_\-"]+$/.test(value);
+          },
+          message: 'Unesite validnu email adresu!'
+        },
+        required: [true, 'Obavezno je unijeti lozinku!'] 
+        
+      }, 
       kategorije: [
-        { naziv: String }
+        { 
+          naziv: {
+            type: String,
+            validate: {
+              validator: function(value){
+                return /^([a-z][0-9]*){3,19}$/.test(value);
+              }
+            }
+          },  
+        }
       ],
       racuni: [
-        { naziv: String, 
+        { 
+          naziv: {
+            type: String,
+            validate: {
+              validator: function(value){
+                return /^([a-z][0-9]*){3,19}$/.test(value);
+              }
+            }
+          },  
           trenutniIznos: Number,
           troskovi: [
             { iznos: Number,
               datum: Date,
-              kategorija: {naziv: String}
+              kategorija: 
+              {
+                naziv: {
+                  type: String,
+                  validate: {
+                    validator: function(value){ //provjera da li postoji kategorija definisana ranije od tog korisnika
+                      if(this.parent().kategorije.findIndex(kat => {return kat.naziv == value})==-1)
+                        return false;
+                      return true;
+                    }
+                  }
+                },  
+              }
             }
           ]
         }
@@ -53,13 +119,16 @@ routerAPI.get('/vratiKorisnika/:ime/:prezime/:lozinka', function(req, res) {
 
 
 app.use(bodyParser.json());
+
 routerAPI.post('/dodajNovuKategoriju/:ime/:prezime/:lozinka', function(req, res) {
   var nazivKategorije = req.body.naziv;
   var ime = req.params.ime;
   var prezime = req.params.prezime;
   var lozinka = req.params.lozinka;
+  
+  var opts = { runValidators: true, context: 'query', new: true };
 
-  korisnik.findOneAndUpdate({'ime':ime, 'prezime': prezime, 'lozinka': lozinka}, {$push:{'kategorije': {'naziv': nazivKategorije}}}, {new: true}, 
+  korisnik.findOneAndUpdate({'ime':ime, 'prezime': prezime, 'lozinka': lozinka}, {$push:{'kategorije': {'naziv': nazivKategorije}}}, opts, 
   function(err, doc){
     if (err) return res.send(500, { error: err });
     return res.send("succesfully saved");
@@ -119,12 +188,14 @@ routerAPI.post('/dodajNoviTrosak/:ime/:prezime/:lozinka/:racun', function(req, r
   var kategorija = req.body.kategorija;
   var danas = new Date();
 
-  //kompleksniji query ako nekom bude trebalo za rutu, 
-  //traži se korisnik s imenom, prezimenom i lozinkom koji su poslani kao parametri u GET metodi,
-  // onda se odabire racun koji ima naziv kao sto je poslan parametar zatim se push-a novi objekat
-  // na listu racuna koji je odabran(za to sluzi ovaj operator $ da bi se odabrao taj nadjeni racun)
+  var opts = { runValidators: true, context: 'query', new: true };
+
+  /*kompleksniji query ako nekom bude trebalo za rutu, traži se korisnik s 
+  imenom, prezimenom i lozinkom koji su poslani kao parametri u GET metodi, onda se odabire racun koji ima naziv
+  kao sto je poslan parametar ('racuni.naziv': racun) zatim se push-a novi objekat na listu racuna koji je 
+  odabran(za to sluzi ovaj operator $ da bi se odabrao taj nadjeni racun iz prethodnog koraka)*/
   korisnik.findOneAndUpdate({'ime':ime, 'prezime': prezime, 'lozinka': lozinka, 'racuni.naziv': racun},
-  {$push:{'racuni.$.troskovi': {'iznos': iznos, 'datum': danas, 'kategorija': {'naziv': kategorija}}}}, {new: true}, 
+  {$push:{'racuni.$.troskovi': {'iznos': iznos, 'datum': danas, 'kategorija': {'naziv': kategorija}}}}, opts, 
   function(err, doc){
     if (err) return res.send(500, { error: err });
     return res.send("Dodan novi trošak");
